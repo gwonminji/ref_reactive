b861cc2ccd8cf11da143119e80f00ad1ffd8ae59
gwonminji <gmj@kipis.co.kr>
2023-01-30 10:36:48 +0900

    ref와 reactive 예제

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,86 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <!--
+      * ref와 reactive의 차이점
+      ref : String, Number, Object 등 어떠한 타입에서도 사용 가능
+      reactive : Object, Array, Map, Set과 같은 타입에서만 사용 가능
+    -->
+    <div class="name">{{ name }}</div>
+    <button type="button" @click="updateName">Click</button>
+    <div class="name2">{{ name2 }}</div>
+    <button type="button" @click="updateName2">+ Click</button>
+    <button type="button" @click="updateName3">- Click</button>
+
+    <div class="cnt">{{ cnt }}</div>
+    <button type="button" @click="onClickAdd">+++</button>
+    
+    <br><br>
+    <div>{{ age }}</div>
   </div>
 </template>
 
 <script>
+import { ref, reactive, computed } from 'vue'
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  // props: {
+  //   msg: String
+  // },
+  setup() {
+    const name = ref('Summer');
+
+    const updateName = () => {
+      name.value = "Yeonsu"
+    }
+
+    const name2 = reactive({
+      id: 1,
+    })
+
+    let cnt = ref(0);
+
+    let age = ref(17);
+
+    const updateName2 = () => {
+      if(name2.id == 10){
+        alert("최대 10");
+        return;
+      }
+      name2.id = name2.id + 1;
+    }
+
+    const updateName3 = () => {
+      if(name2.id == 0){
+        return;
+      }
+      name2.id = name2.id - 1;
+    }
+
+    const onClickAdd = () => {
+      cnt.value++
+      console.log(cnt.value)
+    }
+
+    // const getAge = (computed) => {
+    //   return age.value >= 18? "Yes" : "No";
+    // }
+    const getAge = computed(() => {
+      return age.value >= 18? "Yes" : "No";
+    })
+    console.log(getAge, getAge.value);
+
+    return{
+      name,
+      updateName,
+      name2,
+      updateName2,
+      updateName3,
+      cnt,
+      onClickAdd,
+      age,
+      getAge
+    }
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
